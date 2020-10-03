d9bd8dcb3865355fcd87f4a9da0df005e6fb384a
Unconscious <reverendo_siegfried@live.com.mx>
2020-10-02 20:11:29 -0500

    Componentes añadidos
    creada la carpeta de assets con imagenes donde se referiran las imagenes externas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MoodComponent } from './mood/mood.component';
+import { HomeComponent } from './home/home.component';
+import { DetailComponent } from './detail/detail.component';
+import { AdminLoginComponent } from './admin-login/admin-login.component';
+import { AdminComponent } from './admin/admin.component';
+import { SuperAdminComponent } from './super-admin/super-admin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MoodComponent,
+    HomeComponent,
+    DetailComponent,
+    AdminLoginComponent,
+    AdminComponent,
+    SuperAdminComponent
   ],
   imports: [
     BrowserModule,
